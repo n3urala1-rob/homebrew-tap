c943c28cb64b983f909a95e72a4c069120586364
vf-robert <rob84heine@googlemail.com>
2026-07-05 21:36:29 +0200

    chore: move tap to n3urala1-rob, point cask at a1-pro-orc

--- Casks/pro-orc.rb
+++ Casks/pro-orc.rb
@@ -2,10 +2,10 @@ cask "pro-orc" do
   version "1.2.0"
   sha256 "3b329249d494a968925bde095660d73a835971f268a19049b05da7fc233f894c"
 
-  url "https://github.com/mellow-rob/pro_orc/releases/download/v#{version}/ProOrc-#{version}-macOS.dmg"
+  url "https://github.com/n3urala1-rob/a1-pro-orc/releases/download/v#{version}/ProOrc-#{version}-macOS.dmg"
   name "Pro Orc"
   desc "macOS menubar dashboard for project orchestration"
-  homepage "https://github.com/mellow-rob/pro_orc"
+  homepage "https://github.com/n3urala1-rob/a1-pro-orc"
 
   app "pro_orc.app"
 
