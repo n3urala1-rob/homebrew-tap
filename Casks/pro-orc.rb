27901801dbf7feaae74e1c820414b9de23e937f8
vf-robert <rob84heine@googlemail.com>
2026-08-24 14:34:08 +0200

    pro-orc 3.4.0

--- Casks/pro-orc.rb
+++ Casks/pro-orc.rb
@@ -1,6 +1,6 @@
 cask "pro-orc" do
-  version "3.3.1"
-  sha256 "7a10317a923314cf591870b3d7b2883955fa797a90ca0a2ad0d862e4fad2f80b"
+  version "3.4.0"
+  sha256 "1f378ae383c0f54448caca64c7961766031e2566c02a90751638dd2bf51d8fa1"
 
   url "https://github.com/n3urala1-rob/a1-pro-orc/releases/download/v#{version}/ProOrc-#{version}-macOS.dmg"
   name "Pro Orc"
